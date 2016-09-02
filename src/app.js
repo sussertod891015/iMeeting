ca9cafcc08adee7436980c661737735f1b89230b
Sussertod <1037893409@qq.com>
2016-09-02 14:21:06 +0800

    安装react-redux

---
+++ src/app.js
@@ -0,0 +1,26 @@
+/*
+ * @Author: Sussertod
+ * @Date:   2016-09-02 12:45:03
+ * @Last Modified by:   Sussertod
+ * @Last Modified time: 2016-09-02 14:18:24
+ */
+
+'use strict';
+import React, {
+    Component
+} from 'react';
+import ReactDOM from 'react-dom';
+import 'normalize.css'
+import {
+    connect
+} from 'react-redux';
+
+console.log(Router);
+
+export class Counter extends Component {
+    render() {
+        return (
+            <div></div>
+        );
+    }
+}
